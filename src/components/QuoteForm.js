4d45c96a472d50c9547102b87ebf8819931325bc
Bry Cox <carl.cc@gmail.com>
2020-04-13 15:06:45 +0000

    Automatically backed up by Learn

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -15,6 +15,9 @@ class QuoteForm extends Component {
 
   handleOnChange = event => {
     // Handle Updating Component State
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
   }
 
   handleOnSubmit = event => {
