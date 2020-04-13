99842f697b5e059eee92a5a8cec7c024a2116cb2
Bry Cox <carl.cc@gmail.com>
2020-04-13 15:01:31 +0000

    Automatically backed up by Learn

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -8,9 +8,9 @@ class QuoteForm extends Component {
   state = {
     //set up a controlled form with internal state
     content: '',
-    author: '',
-    votes: 0,
-    id: uuid(),
+    //author: '',
+    //votes: 0,
+    //id: uuid(),
   }
 
   handleOnChange = event => {
